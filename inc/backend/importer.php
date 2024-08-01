9ae926fb7d923128e74dd2f7fc2b42328bb75502
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-08-01 11:04:03 +0500

    sdfsdfsdf

--- inc/backend/importer.php
+++ inc/backend/importer.php
@@ -33,6 +33,25 @@ function restimo_importer() {
 				'primary'   => 'main-menu',
 			)
 		),
+		array(
+			'name'       => 'Restaurant 1',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/restaurant/home1.jpg',
+			'content'    => get_template_directory_uri().'/inc/backend/data/restaurant/demo-content.xml',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/restaurant/customizer.dat',
+			'widgets'    => get_template_directory_uri().'/inc/backend/data/restaurant/widgets.wie',
+			'sliders'    => get_template_directory_uri().'/inc/backend/data/restaurant/sliders.zip',
+			'pages'      => array(
+				'front_page' => 'Home',
+				'blog'       => 'Blog',
+				'shop'       => 'Shop',
+				'cart'       => 'Cart',
+				'checkout'   => 'Checkout',
+				'my_account' => 'My Account',
+			),
+			'menus'      => array(
+				'primary'   => 'main-menu',
+			)
+		),
 	);
 }
 
