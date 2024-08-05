dbd5f5646c884f7e8477c33c6e82f6a33e00a474
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-08-05 14:02:01 +0500

    sdfsdfdsf

--- inc/backend/importer.php
+++ inc/backend/importer.php
@@ -52,6 +52,25 @@ function restimo_importer() {
 				'primary'   => 'main-menu',
 			)
 		),
+		array(
+			'name'       => 'Coffee',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/coffee/home1.jpg',
+			'content'    => get_template_directory_uri().'/inc/backend/data/coffee/demo-content.xml',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/coffee/customizer.dat',
+			'widgets'    => get_template_directory_uri().'/inc/backend/data/coffee/widgets.wie',
+			'sliders'    => get_template_directory_uri().'/inc/backend/data/coffee/sliders.zip',
+			'pages'      => array(
+				'front_page' => 'Home',
+				'blog'       => 'Blog',
+				'shop'       => 'Shop',
+				'cart'       => 'Cart',
+				'checkout'   => 'Checkout',
+				'my_account' => 'My Account',
+			),
+			'menus'      => array(
+				'primary'   => 'main-menu',
+			)
+		),
 	);
 }
 
