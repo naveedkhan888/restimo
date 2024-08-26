43a188b9e71b21c23d0a4b58ceafefc87cce0ce2
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-08-26 13:28:06 +0500

    sdfsdf

--- inc/frontend/template-tags.php
+++ inc/frontend/template-tags.php
@@ -221,12 +221,12 @@ if ( ! function_exists( 'restimo_comment_list' ) ) :
     }
 endif;
 
-//Generate custom search form
+// Generate custom search form
 function restimo_search_form( $form ) {
     $form = '<form role="search" method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '" >
-    <label><span class="screen-reader-text">Search for:</span>
+    <label><span class="screen-reader-text">' . _x( 'Search for:', 'label', 'restimo' ) . '</span>
     <input type="search" class="search-field" placeholder="' . esc_attr__( 'Search&hellip;', 'restimo' ) . '" value="' . get_search_query() . '" name="s" /></label>
-	<button type="submit" class="search-submit"><i class="xp-flaticon-search-1"></i></button>
+    <button type="submit" class="search-submit"><i class="xp-flaticon-search-1"></i></button>
     </form>';
 
     return $form;
