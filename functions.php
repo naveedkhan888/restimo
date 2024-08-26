16517a43bebfce6edcc413f9079ed6e0ac995b68
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-08-26 15:02:36 +0500

    sdfsdf

--- functions.php
+++ functions.php
@@ -302,11 +302,3 @@ add_theme_support('custom-background', array(
 
 add_theme_support('align-wide');
 
-// Define the function to include template parts
-function my_get_template_part( $slug ) {
-    $file = get_template_directory() . $slug . '.php';
-
-    if ( file_exists( $file ) ) {
-        require_once $file;
-    }
-}
